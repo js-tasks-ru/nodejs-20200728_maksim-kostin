58e4b3e5676871fa9b30c4683b222ffd6beb11cf
Максим Костин <koltonweb@mail.ru>
2020-08-06 19:11:37 +0300

    Added task Стрим с лимитом передачи данных

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,11 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.#limit = options.limit;
   }
+
+  #totalLength = 0;
+  #limit = null;
 
   _transform(chunk, encoding, callback) {
-
+    this.#totalLength += chunk.length;
+    if (this.#totalLength > this.#limit) {
+      return callback(new LimitExceededError());
+    }
+    callback(null, chunk);
   }
 }
+
 
 module.exports = LimitSizeStream;

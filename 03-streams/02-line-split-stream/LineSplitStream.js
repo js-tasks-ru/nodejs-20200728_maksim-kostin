751e004414bfb36a119fddb1be92b9b6475a4313
Максим Костин <koltonweb@mail.ru>
2020-08-08 20:05:32 +0300

    Added task Стрим с лимитом передачи данных

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -6,11 +6,23 @@ class LineSplitStream extends stream.Transform {
     super(options);
   }
 
+  #newLineBuf = [];
+
   _transform(chunk, encoding, callback) {
+    this.#newLineBuf.push(chunk);
+    callback();
   }
 
   _flush(callback) {
+    Buffer.concat(this.#newLineBuf)
+    .toString()
+    .split(os.EOL)
+    .forEach(l => {
+        this.push(l);
+      });
+    callback();
   }
 }
+
 
 module.exports = LineSplitStream;

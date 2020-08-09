ea2165cb69df454c566e37f8df2733ecdc28a469
Максим Костин <koltonweb@mail.ru>
2020-08-09 16:04:45 +0300

    Added task Стрим, разбивающий текст на строки

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -6,23 +6,26 @@ class LineSplitStream extends stream.Transform {
     super(options);
   }
 
-  #newLineBuf = [];
+  #lineBuff = '';
 
   _transform(chunk, encoding, callback) {
-    this.#newLineBuf.push(chunk);
+    const isHasEOL = chunk.toString()
+                    .includes(os.EOL);
+    this.#lineBuff += chunk.toString();
+    if (isHasEOL) {
+      this.#lineBuff = this.#lineBuff.split(os.EOL);
+      const pop = this.#lineBuff.pop();
+      this.#lineBuff.forEach(line => {
+        this.push(line);
+      });
+      this.#lineBuff = pop;
+    }
     callback();
   }
 
   _flush(callback) {
-    Buffer.concat(this.#newLineBuf)
-    .toString()
-    .split(os.EOL)
-    .forEach(l => {
-        this.push(l);
-      });
-    callback();
+    callback(null, this.#lineBuff);
   }
 }
-
 
 module.exports = LineSplitStream;
